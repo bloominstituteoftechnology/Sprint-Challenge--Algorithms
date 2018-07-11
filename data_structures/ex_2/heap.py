27f25be83aeba78ea70fbd97d72e4c800b4fd9c5
Sean Chen <skypemaster007@gmail.com>
2018-07-11 14:53:18 -0500

    Add updated heap implementation

--- data_structures/ex_2/heap.py
+++ data_structures/ex_2/heap.py
@@ -1,60 +1,45 @@
-from math import floor
-
 def heapsort(arr):
   pass
 
 class Heap:
   def __init__(self):
-    self.storage = []
+    self.storage = [0]
+    self.size = 0
 
   def insert(self, value):
     self.storage.append(value)
-    self._bubble_up(len(self.storage) - 1)
+    self.size += 1
+    self._bubble_up(self.size)
 
   def delete(self):
-    if not len(self.storage):
-      return None
-    if len(self.storage) == 1:
-      return self.storage.pop()
-    max_value = self.storage[0]
-    self.storage[0] = self.storage.pop()
-    self._sift_down(0)
-    return max_value
+    retval = self.storage[1]
+    self.storage[1] = self.storage[self.size]
+    self.size -= 1
+    self.storage.pop()
+    self._sift_down(1)
+    return retval 
 
   def get_max(self):
-    return self.storage[0]
+    return self.storage[1]
 
   def get_size(self):
-    return len(self.storage)
+    return self.size
 
   def _bubble_up(self, index):
-    parent_index = floor((index - 1) / 2) if index > 0 else 0
-    if self.storage[parent_index] < self.storage[index]:
-      self.storage[parent_index], self.storage[index] = self.storage[index], self.storage[parent_index]
-      self._bubble_up(parent_index)
+    while index // 2 > 0:
+      if self.storage[index // 2] < self.storage[index]:
+        self.storage[index], self.storage[index // 2] = self.storage[index // 2], self.storage[index]
+      index = index // 2
 
   def _sift_down(self, index):
-    left_index = index * 2 + 1
-    right_index = index * 2 + 2
-    max_child_index = len(self.storage) - 1 
-    
-    try:
-      left_child = self.storage[left_index]
-    except IndexError:
-      left_child = None
+    while (index * 2) <= self.size:
+      mc = self._max_child(index)
+      if self.storage[index] < self.storage[mc]:
+        self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+      index = mc
 
-    try:
-      right_child = self.storage[right_index]
-    except IndexError:
-      right_child = None
-
-    if left_child and right_child:
-      max_child_index = left_index if left_child > right_child else right_index
-    elif left_child:
-      max_child_index = left_index
-    elif right_child:
-      max_child_index = right_child
-
-    if self.storage[index] < self.storage[max_child_index]:
-      self.storage[max_child_index], self.storage[index] = self.storage[index], self.storage[max_child_index]
-      self._sift_down(max_child_index)
+  def _max_child(self, index):
+    if index * 2 + 1 > self.size:
+      return index * 2
+    else:
+      return index * 2 if self.storage[index * 2] > self.storage[index * 2 + 1] else index * 2 + 1
