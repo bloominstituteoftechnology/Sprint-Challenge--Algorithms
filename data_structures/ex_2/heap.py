2ecf10d739dc6eeaca67dac63390c00f8d5461f5
Sean Chen <skypemaster007@gmail.com>
2018-08-07 10:08:59 -0500

    Update heap implementation

--- data_structures/ex_2/heap.py
+++ data_structures/ex_2/heap.py
@@ -1,45 +1,42 @@
 def heapsort(arr):
-  pass
+ pass 
 
 class Heap:
   def __init__(self):
-    self.storage = [0]
-    self.size = 0
-
+    self.storage = []
+    
   def insert(self, value):
     self.storage.append(value)
-    self.size += 1
-    self._bubble_up(self.size)
+    self._bubble_up(len(self.storage) - 1)
 
   def delete(self):
-    retval = self.storage[1]
-    self.storage[1] = self.storage[self.size]
-    self.size -= 1
+    retval = self.storage[0]
+    self.storage[0] = self.storage[len(self.storage) - 1]
     self.storage.pop()
-    self._sift_down(1)
+    self._sift_down(0)
     return retval 
 
   def get_max(self):
-    return self.storage[1]
+    return self.storage[0]
 
   def get_size(self):
-    return self.size
+    return len(self.storage)
 
   def _bubble_up(self, index):
-    while index // 2 > 0:
-      if self.storage[index // 2] < self.storage[index]:
-        self.storage[index], self.storage[index // 2] = self.storage[index // 2], self.storage[index]
-      index = index // 2
+    while (index - 1) // 2 >= 0:
+      if self.storage[(index - 1) // 2] < self.storage[index]:
+        self.storage[index], self.storage[(index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+      index = (index - 1) // 2
 
   def _sift_down(self, index):
-    while (index * 2) <= self.size:
+    while index * 2 + 1 <= len(self.storage) - 1:
       mc = self._max_child(index)
       if self.storage[index] < self.storage[mc]:
         self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
       index = mc
 
   def _max_child(self, index):
-    if index * 2 + 1 > self.size:
-      return index * 2
+    if index * 2 + 2 > len(self.storage) - 1:
+      return index * 2 + 1
     else:
-      return index * 2 if self.storage[index * 2] > self.storage[index * 2 + 1] else index * 2 + 1
+      return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2] else index * 2 + 2
