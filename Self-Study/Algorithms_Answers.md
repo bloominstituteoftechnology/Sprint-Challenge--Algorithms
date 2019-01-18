2fe3fb6758bf3be87d1c8b5b404e06a671717dc6
Kat Fries <katherynejf@gmail.com>
2019-01-18 16:06:46 -0500

    post time

--- Self-Study/Algorithms_Answers.md
+++ Self-Study/Algorithms_Answers.md
@@ -1,1 +1,48 @@
 Add your answers to the Algorithms exercises here.
+# Analysis of Algorithms
+
+## Exercise I
+
+Give an analysis of the running time of each snippet of
+pseudocode with respect to the input size n of each of the following:
+
+```
+a)  O(n)
+```
+b)  O(n^3)
+    sum = 0
+    for (i = 0; i < n; i++)  O(n)
+      for (j = i + 1; j < n; j++) O(n)*O(n)
+        for (k = j + 1; k < n; k++) O(n)*O(n)*O(n)
+          for (l = k + 1; l < 10 + k; l++) 
+            sum++
+```
+
+```
+c)  O(n) the recursion is acting like 1 for loop going from n down to 0.
+```
+
+## Exercise II
+
+Suppose that you have an _n_-story building and plenty of eggs. Suppose also
+that an egg gets broken if it is thrown off floor _f_ or higher, and doesn't get
+broken if dropped off a floor less than floor _f_. Devise a strategy to
+determine the value of _f_ such that the number of dropped eggs is minimized.
+I would do a binary search starting at the 1/2 point and seeing if the egg broke or not.
+  If it broke, I would go down to the floor that is 1/4 of the stories. 
+  If it didn't break. I would go to the floor that is 3/4 of the stories
+etc...
+O(log n)
+def binary_search_recursive(arr, target, low, high):
+  if len(arr) == 0:
+    return -1
+  middle = int((low+high)/2)
+  if len(arr) == 0:
+    return -1 # array empty
+  # TO-DO: add missing if/else statements, recursive calls
+  if target == arr[middle]:
+    return middle
+  elif target < arr[middle]:
+    return binary_search_recursive(arr[:middle], target, 0, middle)
+  else:
+    return binary_search_recursive(arr[middle:], target, 0, middle+1)
--- task1/heap.py
+++ task1/heap.py
@@ -1,46 +1,46 @@
-arr1 = [6, 5, 4, 3, 3, 1, 0]
+
+
 def heapsort(arr):
-  if arr == arr1:
-    print('match')
-    return arr
+  result = []
+  heap = Heap()
+  for x in arr:
+    heap.insert(x)
+  print("HEAP", heap)
+  while heap.get_size() > 0: #O(n)
+    v = heap.delete()
+    result.insert(0, v)
+  return result
   new_heap = []
   sorted_heap = []
-  for el in arr:
-    new_heap.append(el)
-    index = len(new_heap) - 1
-    while (index - 1) // 2 >= 0: # while index > 0 or parent not at head
-      if new_heap[(index - 1) // 2] < new_heap[index]: # if parent < new el
-        # swap parent and new el
-        new_heap[index], new_heap[(index - 1) // 2] = new_heap[(index - 1) // 2], new_heap[index]
-      index = (index - 1) // 2  # index is now parent
-    sorted_heap = [new_heap[index]] + sorted_heap
-  print
-  retval = new_heap[0]  # head aka largest number
-  print(new_heap[0])
-  print('NEW HEAP 0', new_heap)
-  new_heap[0] = new_heap[-1:][0]  # new head is last val in arr
-  new_heap.pop() # pop off last val since it is a duplicate
-  print(new_heap[0])
-  print(index)  # index = 0
-  print(index * 2 + 1, len(new_heap) - 1)
-  print('NEW HEAP 1', new_heap)
-  for el in new_heap:
-    while index * 2 + 1 <= len(new_heap) - 1: # while index 1 < last index
-      if index * 2 + 2 > len(new_heap) - 1:
-        mc = index * 2 + 1  # if 2 > last index ==> max child = 1
-      elif new_heap[index * 2 + 1] > new_heap[index * 2 + 2]:
-        mc = index * 2 + 1  #  elif val 1 > val 2  ==> max child = 1
-      else:                 # return max child = 2
-        mc = index * 2 + 2
-      if new_heap[index] < new_heap[mc]: # if val of new head < max child
-          # swap parent and max child
-          new_heap[index], new_heap[mc] = new_heap[mc], new_heap[index]
-      print(new_heap)
-      return new_heap 
-  #   # return retval
-  # print('NEW HEAP 2', new_heap)
-  return new_heap
-print(heapsort([2, 5, 3, 0, 6, 1, 4]))
+  l = len(arr)
+  def grow_heap(arr):
+    print('ARR', arr)
+    for el in arr:
+      new_heap.append(el)
+      index = len(new_heap) - 1
+      while (index - 1) // 2 >= 0: # while index > 0 or parent not at head
+        if new_heap[(index - 1) // 2] < new_heap[index]: # if parent < new el
+          # swap parent and new el
+          new_heap[index], new_heap[(index - 1) // 2] = new_heap[(index - 1) // 2], new_heap[index]
+        index = (index - 1) // 2  # index is now parent
+    print('NEW', new_heap)
+    return new_heap
+  next_heap = grow_heap(arr)
+  # head = next_heap[0]
+  # def get_head(old, new):
+  #   head = old[0]
+  #   new.append(head)
+  #   print('old', old)
+  #   print('new', new)
+  #   print(old, new)
+  #   if len(old) == 1:
+  #     return new
+  #   print('OLD', old)
+  #   return grow_heap(old[1:])
+  # print('NEXT', next_heap)
+  # get_head(next_heap[1:], [head])
+  # # print(x)
+# print(heapsort([2, 1, 3, 6, 10, 5, 7, 0, 8, 9, 4]))
 
   # def _sift_down(self, index):
   #   while index * 2 + 1 <= len(self.storage) - 1:
@@ -102,3 +102,45 @@ class Heap:
       return index * 2 + 1
     else:
       return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2] else index * 2 + 2
+
+  # # while len(sorted_heap) < l:
+  # #   print(len(sorted_heap))
+  # def get_heap_head(new_heap):
+  #   index = 0
+  #   retval = new_heap[0]  # head aka largest number
+  #   print('NEW HEAP 0', new_heap)
+  #   new_heap[0] = new_heap[-1:][0]  # new head is last val in arr
+  #   new_heap.pop() # pop off last val since it is a duplicate
+  #   print(index * 2 + 1, len(new_heap) - 1)
+  #   print('NEW HEAP 1', new_heap)
+  #   for i in range(len(new_heap)):
+  #     while index * 2 + 1 <= len(new_heap) - 1: # while index 1 < last index
+  #       if index * 2 + 2 > len(new_heap) - 1:
+  #         mc = index * 2 + 1  # if 2 > last index ==> max child = 1
+  #       elif new_heap[index * 2 + 1] > new_heap[index * 2 + 2]:
+  #         mc = index * 2 + 1  #  elif val 1 > val 2  ==> max child = 1
+  #       else:                 # return max child = 2
+  #         mc = index * 2 + 2
+  #       if new_heap[index] < new_heap[mc]: # if val of new head < max child
+  #           # swap parent and max child
+  #           new_heap[index], new_heap[mc] = new_heap[mc], new_heap[index]
+  #       print('HEAP', new_heap)
+
+  #       print('RETVAL', retval)
+  #       return retval 
+    
+  #   return retval
+  # head = get_heap_head(new_heap)
+  # sorted_heap = [head] + sorted_heap
+  # print('SORTED', sorted_heap)
+  # while len(sorted_heap) < l:
+  #   print('SORTED', sorted_heap)
+  #   head = get_heap_head(new_heap)
+  #   sorted_heap = [head] + sorted_heap
+  #   get_heap_head(new_heap)
+  # return sorted_heap
+  # print('NEW HEAP 2', new_heap)
+  # # if new_heap[1] > new_heap[0]:
+  # #   new_heap[1], new_heap[0] = new_heap[0], new_heap[1]
+  # print(sorted_heap)
+  # return sorted_heap + new_heap
