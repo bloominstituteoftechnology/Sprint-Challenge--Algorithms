3e7a5abaa8ebe73744b60c928d88540d198f8446
Brock Maraee Rohloff <00AgentB@gmail.com>
2019-01-18 11:33:27 -0700

    Finish Min Heapsort Refactor

--- task1/heap.py
+++ task1/heap.py
@@ -38,6 +38,46 @@ class Heap:
     else:
       return index * 2 + 1 if self.storage[index * 2 + 1] > self.storage[index * 2 + 2] else index * 2 + 2
 
+class Min_Heap:
+  def __init__(self):
+    self.storage = []
+    
+  def insert(self, value):
+    self.storage.append(value)
+    self._bubble_up(len(self.storage) - 1)
+
+  def delete(self):
+    retval = self.storage[0]
+    self.storage[0] = self.storage[len(self.storage) - 1]
+    self.storage.pop()
+    self._sift_down(0)
+    return retval 
+
+  def get_max(self):
+    return self.storage[0]
+
+  def get_size(self):
+    return len(self.storage)
+
+  def _bubble_up(self, index):
+    while (index - 1) // 2 >= 0:
+      if self.storage[(index - 1) // 2] > self.storage[index]:
+        self.storage[index], self.storage[(index - 1) // 2] = self.storage[(index - 1) // 2], self.storage[index]
+      index = (index - 1) // 2
+
+  def _sift_down(self, index):
+    while index * 2 + 1 <= len(self.storage) - 1:
+      mc = self._max_child(index)
+      if self.storage[index] > self.storage[mc]:
+        self.storage[index], self.storage[mc] = self.storage[mc], self.storage[index]
+      index = mc
+
+  def _max_child(self, index):
+    if index * 2 + 2 > len(self.storage) - 1:
+      return index * 2 + 1
+    else:
+      return index * 2 + 1 if self.storage[index * 2 + 1] < self.storage[index * 2 + 2] else index * 2 + 2
+
 def heapsort(arr):
 
   # Declare a new list to store our sorted data into:
@@ -54,5 +94,25 @@ def heapsort(arr):
 
   # Finally, we return our list, now with the elements sorted.
   return sorted
+
+def min_heapsort(arr):
+  
+  # Declare a new list to store our sorted data into:
+  sorted = []
  
-# print(heapsort([15, 19, 10, 7, 17, 16, 12341, 34, 456, 76, 123]))
+  # First, let's 'heapify' our array data:
+  heap = Min_Heap()
+  for i in arr:
+    heap.insert(i)    
+
+  # Next, we should iteratively remove the top node of the heap and add it to our sorted list until the heap is empty:
+  for i in range(0, heap.get_size()):
+    sorted = sorted + [heap.delete()]
+
+  # Finally, we return our list, now with the elements sorted.
+  return sorted
+
+test_array = [15, 19, 10, 7, 17, 16, 12341, 34, 456, 76, 123]
+
+print(heapsort(test_array))
+print(min_heapsort(test_array))
