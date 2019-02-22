5deb8ddafd8cdd01ad8817a2361a6388d5caa2a2
Carlos Lantigua <clantigua954@gmail.com>
2019-02-22 13:23:44 -0500

    working on duplicate numbers currently I set 60 and its working but not picking up on

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -117,6 +117,9 @@ class SortingRobot:
             a. move the robot left and grab that item.
             b. move the robot right until it finds a 1 and drop that item.
             c. continue right to check next item.
+        3.  While the light is on and the compared item is equal expect to get 0 from the compare method.
+            a. nothing should be done in this case??
+            b. Ignore the items and just move the robot forward.
         """
         while not self.light_is_on():
             self.set_light_on()
@@ -136,13 +139,23 @@ class SortingRobot:
                     self.move_left()
                     self.swap_item()
                     self.move_right()
+                # step3 (a) = if the current item returns as 0
+                if self.compare_item() == 0:
+                    self.move_right()
+                    self.swap_item()
+                    self.move_right()
+                    self.swap_item()
+                    self.move_right()
+            if not self.light_is_on():
+                while self.can_move_left():
+                    self.move_left()
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [41, 15, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+    l = [41, 15, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 60, 1,
          45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
