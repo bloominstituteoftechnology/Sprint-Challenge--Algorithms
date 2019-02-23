715ae8348103f47cc5bd408c8ae3a147234da06a
Troy Amlee <troyamlee19@gmail.com>
2019-02-22 19:11:10 -0600

    [mvp] prior solution had illegal methods

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -102,79 +102,48 @@ class SortingRobot:
         """
         # Fill this out
         self.light_is_on()
-        while self.light_is_on() == False:
+        self.set_light_on()
+        while self.can_move_right() == True:
             if self.compare_item() == None:
+                # print("##############################")
                 self.swap_item()
                 self.move_right()
             if self.compare_item() == -1:
+                # print("#######$%^^^$^%$^^$^$%^#####")
                 self.move_left()
                 self.swap_item()
                 self.move_right()
             if self.compare_item() == 1:
+                # print("####&*(&(*&*(*&*(*(&((###")
                 self.swap_item()
                 self.move_left()
-                # self.set_light_on()
+                self.set_light_on()
                 print("When light is off: ",l)
+
+
+            # if self.compare_item() is not -1 and 1:
+            #     print('Doh')
             
             elif self.can_move_right() == False:
 
-                print(l)
-                self.set_light_on()
+                # print(l)
+                
                 # print("whatsss")
                 while self.light_is_on() == True:
                     self.move_left()
-                    print("what in here")
+                    print("whats in here")
                     if self.can_move_left() == False:
                 #         # self.swap_item()
                 #         print("what")
-                        def is_sorted(lst):
-                            for i, el in enumerate(lst[1:]):
-                                if el < lst[i]: # i is the index of the previous element
-                                    return False
-                            return True
-                        if is_sorted(l) == True:
-                            return l
-                        else:
-                            self.set_light_off()
-
- 
-
-            # while self.light_is_on() == True:
-                # if self.compare_item() == None:
-                #     self.move_left()
-                # if self.compare_item() == -1:
-                #     self.swap_item()
-                #     self.move_left()
-                #     print("When light is on 1: ",l)
-                # if self.compare_item() == 1:
-                #     self.move_right()
-                #     self.swap_item()
-                #     self.move_left()
-                #     print("When light is on 2: ",l)
-                
-                # if self.compare_item() == 1:
-                #     self.move_left()
-                #     if self.compare_item() == 1:
-                #         self.swap_item()
-                #         self.move_right()
-                #     elif self.compare_item() == -1:
-                #         self.move_right()
-
-                # if self.can_move_left() == False:
-                #     while self.compare_item() != None:
-                #         if self.compare_item() == 1:
-                #             self.move_right()
-                #         if self.compare_item() == -1:
-                #             self.swap_item()
-                #             self.move_right()
-                #     if self.compare_item() == None:
-                #         self.swap_item()
-                #         self.move_right()
-                #         self.set_light_off()
-
-                
-
-
+                        # def is_sorted(lst):
+                        #     for i in lst[0:]:
+                        #         for j in lst[1:]: # i is the index of the previous element
+                        #             print(i,j)
+                        #     return True
+                        # if is_sorted(l) == True:
+                        #     return l
+                        # else:
+                        self.set_light_off()
 
         
 
@@ -183,7 +152,8 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 99, 26, 4, 28, 8, 44, 45]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+
 
     robot = SortingRobot(l)
 
