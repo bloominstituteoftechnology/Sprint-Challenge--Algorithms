e18fa3530b8b20ab8a2adcae104f85e64f090a46
Carlos Gutierrez <chgutier@gmail.com>
2019-06-07 10:32:53 -0500

    Starting to work on SC

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,8 +96,25 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        # Light on once the robot begins to sort
+
+            while self.can_move_right():
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+
+            while self.can_move_left():
+                self.swap_item()
+                self.move_left()
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.set_light_on()
+                self.move_right()
+                self.swap_item()
+                self.move_left()
+        # Light off once the robot is done sorting
+        self.set_light_off()
 
 
 if __name__ == "__main__":
