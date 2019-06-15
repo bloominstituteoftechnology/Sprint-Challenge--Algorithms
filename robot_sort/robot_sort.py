8cd3252ad1b21f4e440488db0c9c0bb02670b861
Matthew Meitl <darksamus6@gmail.com>
2019-06-15 15:10:50 -0500

    changing the order fixed the sorting issue;

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -99,7 +99,25 @@ class SortingRobot:
         while self.light_is_on() == False:
         
             self.set_light_on()
+            
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_off()
+
+                self.move_left()
+                self.swap_item()
+                self.move_right() 
+  
+
+            if self.light_is_on():
+                break      
     
+            self.set_light_on()
+
             while self.can_move_left():
                     
                 self.swap_item()
@@ -111,22 +129,9 @@ class SortingRobot:
                     
                 self.move_right()
                 self.swap_item()
-                self.move_left
+                self.move_left()                    
 
-            if self.light_is_on():
-                break
-
-            while self.can_move_right:
-                self.swap_item()
-                self.move_right()
-
-                if self.compare_item() == 1:
-                    self.swap_item()
-                    self.set_light_off()
-
-                self.move_left()
-                self.swap_item()
-                self.move_right()        
+       
 
                 
             
