d68dab525d39b84c4d3dd374a5d5d7679d15c8ba
Levi Appenfelder <levi.appenfelder@gmail.com>
2019-06-16 00:15:24 -0600

    Robot Sort
    While light_is_on() == False:  keeps the robot sorting until the Light is turned on.
    While self.move_right() == True: keeps the robot moving in the right direction as it swaps with appropriate numbers

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,16 +97,45 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        self.swap_item()
+        self.move_right()
+        while light_is_on() == False:
+            while self.move_right():
+                if self.compare_item() == -1:
+                    self.swap_item()
+                self.move_right()
+            if 
+
+                if self.can_move_right() == False and self.compare_item() == None:
+                    self.set_light_on()
+
+
+            self.move_right()
+        self.set_light_on()
+        self.move_right()
+        self.set_light_off()
+        return print(self.light_is_on())
+    
+
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+            [2]
+    l = [1,None,4,3,5,6]
     robot = SortingRobot(l)
 
     robot.sort()
     print(robot._list)
+
+
+
+    # if item > item in front going right...swap_item
+
+    # if item < item in front going left...swap_item
+
+    #            [5]      
+    # [2,3,None,3,6,7]
