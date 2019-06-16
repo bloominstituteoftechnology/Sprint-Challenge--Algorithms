b0895bea61e523f00cffb23d22a8331e822ed972
Phinehas <kohler.nai@gmail.com>
2019-06-15 22:04:36 -0600

    The list sorts, but the tests take too long. Working on a more efficient way to sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,8 +97,43 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        
+        self.set_light_on()
+        while self.light_is_on():
+            self.set_light_off()
 
+            ## Sorting right
+            while self.can_move_right():                
+                self.swap_item()
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                    self.set_light_on()
+
+                elif self.compare_item() == -1:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                
+
+            ## Sorting left
+            while self.can_move_left():                
+                self.swap_item()
+                self.move_left()
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.move_right()
+                    self.swap_item()
+                    self.move_left()
+                    self.set_light_on()
+                elif self.compare_item() == 1:
+                    self.move_right()
+                    self.swap_item()
+                    self.move_left()
+                    
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
