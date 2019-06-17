a9935c0476cc4f7ee7aa4b278644e36ceb3b0bbe
Chris Roth <c.roth.lambda@gmail.com>
2019-06-16 23:05:17 -0500

    Robot Sort Completed: MVP Complete

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -92,20 +92,40 @@ class SortingRobot:
         """
         return self._light == "ON"
 
+
     def sort(self):
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        while self.can_move_right():
+            #print("before", self._item)
+            if self.compare_item() == -1 or self.compare_item() == None:
+                self.swap_item()
+               # print("after",self._item)
+                self.move_right()
+            else:
+                self.move_right()
+
+        while self.can_move_left():
+            #print("left before", self._item)
+            if self.compare_item() == 1:
+                self.swap_item()
+               # print("left after", self._item)
+                self.move_left()
+            else:
+                self.move_left()
+        
+        if self._item != None:
+            self.sort()
+
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [10, 7, 5, 3, 6]
     robot = SortingRobot(l)
 
     robot.sort()
