5ba76c17397d277a24aa7922dd8fca8411f8ab59
Jennifer King <jenbellpgh@gmail.com>
2019-06-18 00:43:54 -0400

    Last test results in max recursion depth

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -113,26 +113,24 @@ class SortingRobot:
         # Turn light off if swap happens
         # Use light to determine if swaps have taken place
         # How to do this without using a temp variable in for loop?
-        print("Sorting...")
+
         self.swap_item()
         self.set_light_on()
-        #print("light on")
 
         while(self.can_move_right() == True):
-        
+
             # Move forward 1
             self.move_right()
 
             if self.compare_item() == -1:
                 # Held item is lower - move back & then swap
-                #print(f"{self._item} is lower than {self._list[self._position]}")
                 self.move_left()
                 self.swap_item()
                 self.move_right()
-                self.swap_item()
+                if self.can_move_right():
+                    self.swap_item()
             else:
                 # Held item is higher - swap & move lower number back
-                #print(f"{self._item} is higher than {self._list[self._position]}")
                 self.swap_item()
                 self.move_left()
                 self.swap_item()
@@ -141,30 +139,27 @@ class SortingRobot:
 
                 if self.can_move_right():
                     self.swap_item()
-                
-                #print(f"light off: {self._light}")
+                else:
+                    break
+
 
         if self.light_is_on():
             # List is sorted
             #self.return_to_start()
             #self.swap_item()
-            print(self._list)
-            print("Light is on")
             return
         else:
             # Do it again
             self.return_to_start()
-            print("Back to beginning")
-            print(self._list)
-            return self.sort()
+            self.sort()
                 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    #l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [1, -38, -95, 4, 23, -73, -65, -36, 85, 2, 58, -26, -55, 96, 55, -76, 64, 45, 69, 36, 69, 47, 29, -47, 13, 89, -57, -88, -87, 54, 60, 56, -98, -78, 59, 93, -41, -74, 73, -35, -23, -79, -35, 46, -18, -18, 37, -64, 14, -57, -2, 15, -85, 45, -73, -2, 79, -87, -100, 21, -51, 22, 26, -59, 81, 59, -24, 24, -81, 43, 61, 52, 38, -88, -95, 87, -57, -37, -65, -47, -3, 21, -77, 98, 25, 1, -36, 39, 78, 47, -35, -40, -69, -81, 11, -47, 21, 25, -53, -31]
     robot = SortingRobot(l)
 
     robot.sort()
