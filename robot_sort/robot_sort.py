89c85a25027a83e6f202ec062c4498b653697e41
badCompany55 <zachirvin45@gmail.com>
2019-08-31 13:29:48 -0500

    MVP, finished robot sort, mvp complete

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,16 +96,36 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
 
+        while True:
+            self.set_light_off()
+            self.swap_item()
+            while self.move_right():
+                if (self.compare_item() == 1):
+                    self.set_light_on()  
+                    self.swap_item()
+                self.move_left()  
+                self.swap_item()
+                self.move_right()
+                self.swap_item()
+            self.swap_item()
+            if (self.light_is_on() == False):
+                break
+            while self.move_left(): 
+                pass
+
+
+
+
+            
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
-
+    l = [15, 41, 58, 49, 26, 4, 28, 8]
+    # , 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    #
     robot = SortingRobot(l)
 
     robot.sort()
