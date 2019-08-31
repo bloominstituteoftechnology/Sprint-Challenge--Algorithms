9d92af61795bac5697ba001ee001d3b7d27471a9
Jonathan Dorety <kyullog@gmail.com>
2019-08-31 09:58:33 -0700

    pipenv added deps

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,8 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
+    if "th" not in word:
+        return 0
     # TBC
     
     pass
