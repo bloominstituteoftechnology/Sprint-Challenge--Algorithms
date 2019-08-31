0f52dbe638465530714423e31f375e061474bd41
Jonathan Dorety <kyullog@gmail.com>
2019-08-31 11:10:15 -0700

    Finsh count_th recursive solution

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,13 +5,12 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 
 
-def count_th(word):
-    sum = 0
+def count_th(word, sum=0):
+    count = sum
     if "th" not in word:
-        return 0
-    if "th" in word:
-        sum += 1
-        index = word.find("th")
-        count_th(word[index + 2:])
+        return count
 
-    return sum
+    if "th" in word:
+        index = word.find("th")
+        count += 1
+        return count_th(word[index + 2:], count)
