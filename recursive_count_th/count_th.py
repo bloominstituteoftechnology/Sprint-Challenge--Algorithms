4aced78fa86e7a0348d439776b9422a2ce637b39
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-11 09:17:54 +0200

    Polya for recursive_count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,20 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
+    counter = 0
+
+    # What is the base case?
+        # len(word) <= 2 & word does not include 'th'
+        # Then we should return
+
+    # Since we have to count something, might it be prudent to add a counter outside of a recursive helper function?
+
+    # IDEA
+    # Check if word[0:2] are 'th'
+        # If so, add 1 to counter
     
-    # TBC
+    # Then recursively call the function count_th with word[2:]
     
     pass
+
+print("the"[2:])
