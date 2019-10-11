edd18a8747cb1fe82379d930c58d0ffd90c6ef46
Boris Krant <38628286+bkrant@users.noreply.github.com>
2019-10-11 17:09:06 -0400

    Update count_th.py

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,16 +3,11 @@ Your function should take in a signle parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-from textwrap import wrap
-
 def count_th(word):
-    
-    if word == 'th':
-        return 1
-    elif word != 'th':
+    if len(word) <= 1:
         return 0
-    else:
-        #word_wrap_list = wrap(word, 2)
-        #s1 = sum(map(count_th, wrap(word,2)))
-        #s2 = sum(map(count_th, wrap(word[1:],2)))
-        return sum(map(count_th1, wrap(word,2)))
+
+    if word[:2] == 'th':
+        return count_th(word[1:]) + 1
+
+    return count_th(word[1:])
