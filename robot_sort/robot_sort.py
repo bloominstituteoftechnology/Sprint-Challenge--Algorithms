d8d8f497d4879fbb6c6ee8476abaca9228e60373
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2019-10-11 18:40:05 -0400

    Began attempting to impliment robot sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,3 +1,39 @@
+'''
+Objective: Utilize given methods on robot class to sort through a list.
+Plan :
+The given abilities of the robot most closely resemble the activities of a bubble sort or a selection sort.
+I am able to grab and hold values and compare the values in front of the robot to the ones it is holding.
+I will have to use the move methods to resemble an index looping through an array.
+- the robot will compare the # in front of it to the # to its left and swap them if the # to left is higher than
+the # to its right. It will continue to do so until it has gone through all the #'s and by the end the list it looped
+through will be sorted bubble sort style.
+- new plan will try to implement selection sort instead
+psuedocode
+* Robot can only compare the # he holds to number he is infront of *
+
+for i in range(0, len(list)):
+    index_of_smallest_# = i (Might have to pick up item at this step)
+    #robot has to move along with loop
+
+    for j in range(i + 1, len(list)):
+    #robot has to move along with loop
+
+        if item_holding > item_infront_of_robot:
+            # Re assign the index of the smallest number
+            index_of_smallest_# = j
+
+        #store value of list[i] in temp var
+        temp = list[i]
+        #swap the value of list[i] for the value of the list[index_of_smallest_num]
+        #robot has to perform this swap ...
+        #How can the robot swap these two items.
+        #He has to hold on and be in front of the other one
+        list[i] = list[index_of_smallest_num]
+        arr[index_of_smallest_num] = temp
+
+    return list
+
+'''
 class SortingRobot:
     def __init__(self, l):
         """
@@ -92,12 +128,65 @@ class SortingRobot:
         """
         return self._light == "ON"
 
-    def sort(self):
-        """
-        Sort the robot's list.
-        """
-        # Fill this out
-        pass
+    def sort(self, list):
+        for i in range(0, len(list)):
+            index_of_smallest_num = i #Does the robot have to pick this up? If it picks it up and no item is > how does it have to drop it back off?
+            self.swap_item() # Trying out picking up item in order to figure out how to compare down the list.
+
+            print(f"Robot holding (1st loop) = {robot._item}")
+            print(f"Robot return position (1st loop)= {robot._position}")
+
+            #if i > 0:
+            #self.move_right()
+            print(f"Robot return position + 1 = {robot._position}\n")
+
+            for j in range(i + 1, len(list)):
+                #if j > 1:
+                self.move_right()
+                print(f"Robot index > 0 (2nd loop) = {robot._position}")
+
+                if self.compare_item == 0:
+                    #drop item back off at where it picked it up
+                    pass
+                if self.compare_item() == 1:
+                    # Re assign the index of the smallest number
+                    index_of_smallest_num = j # -> this is the index the robot needs to be in in order to swap with the item its holding.
+                    print(f"SMALLEST # = {list[index_of_smallest_num]}")
+
+                print(f"Swap position : {index_of_smallest_num} current position (2nd loop) = {robot._position}\n")
+                #self.swap_item()
+                #print(f"Robot holding SWAP = {robot._item}")
+
+                temp = list[i]
+                list[i] = list[index_of_smallest_num]
+                list[index_of_smallest_num] = temp
+
+                print(f"Robot holding (2nd loop) = {robot._item}")
+                print(f"Robot index (2nd loop) = {robot._position}")
+
+        return list
+
+
+l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21]
+
+robot = SortingRobot(l)
+
+print(f"{robot.sort(l)}")
+
+'''# store value of list[i] in temp var
+temp = list[i] # -> The value that was picked up at first
+
+# swap the value of list[i] for the value of the list[index_of_smallest_num]
+# robot has to perform this swap ...
+# How can the robot swap these two items.
+# He has to hold one and be in front of the other one
+list[i] = list[index_of_smallest_num]
+print(f"Robot holding (2nd loop) = {robot._item}")
+list[index_of_smallest_num] = temp'''''
+
+#print(f"Robot index = {robot._position}")
+#print(f"Robot holding = {robot._item}")
+#print(f"light is = {robot._light}")
 
 
 '''if __name__ == "__main__":
