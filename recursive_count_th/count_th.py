bf4c38524aa52fc0834f86bbcec83b9b4b192fd5
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-11 09:29:42 +0200

    Refactoring count_th to get rid of superfluous helper function

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,27 +3,20 @@ Your function should take in a signle parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-def count_th(word):
+def count_th(word, counter = 0):
+    # len(word) <= 2, the word does not include 'th'
+    if len(word) < 2:
+        return counter
 
-    def count_th_helper(word, counter):
-        # len(word) <= 2, the word does not include 'th'
-        if len(word) < 2:
-            return counter
+    # Check if word[0:2] are 'th'
+    if word[0:2] == 'th':
+        # If so, add 1 to counter
+        counter += 1
 
-        # Check if word[0:2] are 'th'
-        if word[0:2] == 'th':
-            # If so, add 1 to counter
-            counter += 1
-    
-        # Then recursively call the function count_th with word[1:]
-        # Move up one letter and check if [0:2] are 'th'.
-            # If we care about performance, we could check if word[1] == 't'.
-            # If so, we could move over 1, else 2
-        return count_th_helper(word[1:], counter)
-
-
-
-
-    return count_th_helper(word, 0)
+    # Then recursively call the function count_th with word[1:]
+    # Move up one letter and check if [0:2] are 'th'.
+        # If we care about performance, we could check if word[1] == 't'.
+        # If so, we could move over 1, else 2
+    return count_th(word[1:], counter)
 
 # print(count_th("thetHth"))
