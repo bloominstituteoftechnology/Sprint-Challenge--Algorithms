8d2ca2b41e04c3b56d81352a877e70ee42870f3b
Mitchell Robles <mitchellr92@live.com>
2019-10-11 12:55:05 -0700

    Robot sort works on any list, need to fix infinite loop

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -115,33 +115,40 @@ class SortingRobot:
         self.grab_item()
 
         while self._light == "ON":
+            print("\nnew loop")
+            print(f"current item: {self._item}")
+            print(f"current position: {self._position}")
+            print(f"current list: {self._list}")
             if self.can_move_right():
                 if self._item > self._list[self._position + 1]:
-                    print(self._list)
-                    print(self._list)
-                    print(f"current item: {self._item}")
+                    print(f"set {self._list[self._position]} to {self._list[self._position + 1]} and move one spot to the right")
                     self._list[self._position] = self._list[self._position + 1]
                     self.move_right()
-                elif self._item <= self._list[self._position + 1]:
-                    self.drop_item()
-                    print(f"position: {self._position}")
-                    self._position = self._list[self._position]
-                    print(self._list)
                     print(f"new position: {self._position}")
-                    self.grab_item()
-                    if self.can_move_right():
-                        if self._item > self._list[self._position + 1]:
-                            self._list[self._position] = self._list[self._position + 1]
-                            self.move_right()
-            elif self.can_move_right() == False:
+                    print(f"new list: {self._list}")
+                    print("loop again")
+                    
+                elif self._item <= self._list[self._position + 1]:
+                    print(f"drop {self._item} and move to the right and pick up {self._list[self._position + 1]}")
                     self.drop_item()
-                    self._position = 0
+                    self.move_right()
                     self.grab_item()
-                    print(self._item)
-                    print(self._position)
+                    print(f"new item: {self._item}")
+                    print(f"new position: {self._position}")
+                    print(f"new list: {self._list}")
+                    print("loop again")
+            if self.can_move_right() == False:
+                self.drop_item()
+                self._position = 0
+                self.grab_item()
+                
+                # self.set_light_off()
 
 
-l = [5, 4, 3, 2, 1]
+
+
+
+l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
 robot = SortingRobot(l)
 
