46a39f7955211ce4ee20693618398a8f030292e7
Mitchell Robles <mitchellr92@live.com>
2019-10-11 09:32:44 -0700

    th string counter finshed, works in main file but doesnt pass tests

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,27 @@ Your function should take in a signle parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+count = 0
 def count_th(word):
     
     # TBC
-    
-    pass
+    # print(f"word length: {len(word)}")
+    substr = "th"
+
+    if len(word) < 2:
+        global count
+        return count
+
+    # print(word[2:])
+
+    if word[:2] == substr:
+        count += 1
+        # print(f"count: {count}")
+        return count_th(word[2:])
+
+    return count_th(word[1:])
+
+
+
+
+print(count_th("thetahthehsoeth sootth"))
