22d1aedd1528aba74f80267346a42b0467e4d93f
Alex <statueofibberty@gmail.com>
2019-10-11 12:26:38 -0400

    Finish recursive count

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,9 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
-    
-    pass
+    if len(word) < 2: #impossible, so return 0
+        return 0
+    if word[:2] == 'th': #letters 'th' exist as the first 2 letters in the string, we then add 1 to our counter and continue to check for more occurances
+        return 1 + count_th(word[2:])
+    else: #first 2 letters do not match 'th' so we run the function again to continue checking the rest of the string
+        return 0 + count_th(word[1:])
