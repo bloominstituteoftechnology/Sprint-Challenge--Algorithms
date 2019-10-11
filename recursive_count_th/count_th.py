050bc5d917256a7c3522eb6fa893c9bc02fc1d26
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-11 09:28:33 +0200

    Completed count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,20 +4,26 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    counter = 0
 
-    # What is the base case?
-        # len(word) <= 2 & word does not include 'th'
-        # Then we should return
+    def count_th_helper(word, counter):
+        # len(word) <= 2, the word does not include 'th'
+        if len(word) < 2:
+            return counter
 
-    # Since we have to count something, might it be prudent to add a counter outside of a recursive helper function?
-
-    # IDEA
-    # Check if word[0:2] are 'th'
-        # If so, add 1 to counter
+        # Check if word[0:2] are 'th'
+        if word[0:2] == 'th':
+            # If so, add 1 to counter
+            counter += 1
     
-    # Then recursively call the function count_th with word[2:]
-    
-    pass
+        # Then recursively call the function count_th with word[1:]
+        # Move up one letter and check if [0:2] are 'th'.
+            # If we care about performance, we could check if word[1] == 't'.
+            # If so, we could move over 1, else 2
+        return count_th_helper(word[1:], counter)
 
-print("the"[2:])
+
+
+
+    return count_th_helper(word, 0)
+
+# print(count_th("thetHth"))
