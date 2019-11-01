3fe0a3e3929499d6f43d9ecf0101945d137b33c7
Brian "Beej Jorgensen" Hall <beej@lambdaschool.com>
2019-11-01 09:21:07 -0700

    typo

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,5 +1,5 @@
 '''
-Your function should take in a signle parameter (a string `word`)
+Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
