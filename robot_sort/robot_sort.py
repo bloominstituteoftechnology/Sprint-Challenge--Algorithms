fc14781973749406f8e8c9e38308d99dc025d397
Melanie Harris <mell.j.harris@gmail.com>
2019-11-08 10:38:15 -0800

    MVP met

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,44 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
+        while True:
+            self.set_light_off()
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+        
+                self.move_left()
+                self.swap_item()
+
+                self.move_right()
+
+        if self.light_is_on() == False:
+            return
+            
+        self.set_light_off()
+        self.move_left()
+        while self.can_move_left():
+            self.swap_item()
+            self.move_left()
+
+            if self.compare_item() == -1:
+                self.swap_item()
+                self.set_light_on()
+        
+            self.move_right()
+            self.swap_item()
+            self.move_left()
+
+        if self.light_is_on() == False:
+            return
+            
+        print(self._list)
         pass
+
 
 
 if __name__ == "__main__":
