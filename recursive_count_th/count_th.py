744017ba09f6ad022f033a60a1c4787a5234d12b
standroidbeta <43893890+standroidbeta@users.noreply.github.com>
2019-12-13 13:34:28 -0500

    count_th completed

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,6 +6,9 @@ Your function must utilize recursion. It cannot contain any loops.
 
 
 def count_th(word):
-    # TBC
-
-    pass
+    if len(word) < 2:  # skips words with less than 2 characters.
+        return 0
+    elif word[:2] == 'th':  # checks to see if 'th' is in the first two indexes.
+        return 1 + count_th(word[1:])  # Moves to the next index skipping the first
+    else:
+        return count_th(word[1:])  # just remove the first index of the word w/o incrementing
