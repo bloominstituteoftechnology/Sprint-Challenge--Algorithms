18b4810ab634ccaac9fdf6906ef291d05b2ae8b9
marcusbass323 <bass.marcus@icloud.com>
2019-12-17 20:27:35 -0800

    count done

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -8,3 +8,11 @@ def count_th(word):
     # TBC
     
     pass
+
+    if len(word) < 2:
+        return 0
+
+    if word[:2] == "th":
+        return 1 + count_th(word[2:])
+    else:
+        return 0 + count_th(word[1:])
