da89fe64cb9c11b3b6bd2d06d46c251db2ae893f
Michael Redig <mredig@gmail.com>
2020-01-24 11:59:29 -0600

    robot sort stretch works

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -100,23 +100,42 @@ class SortingRobot:
         """
         self.set_light_on()
         while self.light_is_on():
-            self.set_light_off()
+            self.sortRight()
+            if self.light_is_on():
+                self.sortLeft()
 
-            while self.can_move_right():
+    def sortRight(self):
+        self.set_light_off()
+        while self.can_move_right():
+            self.swap_item()
+            self.move_right()
+            if self.compare_item() == 1:
+                self.swap_item()
+                self.set_light_on()
+                self.move_left()
                 self.swap_item()
                 self.move_right()
-                if self.compare_item() == 1:
-                    self.swap_item()
-                    self.set_light_on()
-                    self.move_left()
-                    self.swap_item()
-                    self.move_right()
-                else:
-                    self.move_left()
-                    self.swap_item()
-                    self.move_right()
+            else:
+                self.move_left()
+                self.swap_item()
+                self.move_right()
 
-            while self.can_move_left() and self.light_is_on():
+    def sortLeft(self):
+        self.set_light_off()
+        # by the time sort left runs, the last item in the array will always have been sorted
+        self.move_left()
+        while self.can_move_left():
+            self.swap_item()
+            self.move_left()
+            if self.compare_item() == -1:
+                self.swap_item()
+                self.set_light_on()
+                self.move_right()
+                self.swap_item()
+                self.move_left()
+            else:
+                self.move_right()
+                self.swap_item()
                 self.move_left()
 
 
