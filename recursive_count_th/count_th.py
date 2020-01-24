283fcac64e8e49a4e547245e68268368e8d79a3f
Jeff <theta_158@yahoo.com>
2020-01-24 12:15:19 -0500

    added comments to count_th.py for clarity

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,11 +7,14 @@ Your function must utilize recursion. It cannot contain any loops.
 
 def count_th(word):
 
+    # Base case: a word much have 2 or more letters
     if len(word) < 2:
         return 0
 
-    # Check pairs
+    # Check first two letters of word for 'th'
     elif word[0:2] == 'th':
+        # Recursive call that slices the first 2 letters
+        # off the word each time while counting each occurance of th
         return count_th(word[1:]) + 1
     else:
         return count_th(word[1:])
