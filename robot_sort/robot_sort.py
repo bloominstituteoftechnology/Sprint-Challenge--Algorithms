0f280648c6ef2cfe2957d32a27f601357c490522
Michael Redig <mredig@gmail.com>
2020-01-24 11:47:31 -0600

    setup robot sort with bubble sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -98,8 +98,26 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        self.set_light_on()
+        while self.light_is_on():
+            self.set_light_off()
+
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+                else:
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
+
+            while self.can_move_left() and self.light_is_on():
+                self.move_left()
 
 
 if __name__ == "__main__":
