95dd82413555ba7a993dcabca4f8ac95c8e11c42
jkaunert <44586402+jkaunert@users.noreply.github.com>
2020-01-24 13:56:39 -0600

    (feat) count_th.py functioning and meets MVP

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,13 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    #base case, word length is too short
+    if len(word) < 2:
+        return  0 # can't have a "th" with less than 2 characters now can we?
+    else:
+        # we found a "th"
+        if word[:2] == "th":
+            return count_th(word[1:]) + 1
+        else: # we don't find a "th"
+            return count_th(word[1:])
+
