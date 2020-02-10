be8881d79e30247cab2c03be0239bfd737d71818
Victor Ruiz <ruizdvictor@gmail.com>
2020-02-10 01:47:59 -0500

    added sorting method for robot

--- robot_sort/test_robot.py
+++ robot_sort/test_robot.py
@@ -1,6 +1,7 @@
 import unittest
 import random
-from robot_sort import SortingRobot
+from robot_sort import *
+
 
 class Test(unittest.TestCase):
 
@@ -15,7 +16,6 @@ class Test(unittest.TestCase):
         robot = SortingRobot(self.small_list)
         robot.sort()
         self.assertEqual(robot._list, sorted(self.small_list))
-
     def test_sorting_medium_list(self):
         robot = SortingRobot(self.medium_list)
         robot.sort()
@@ -35,26 +35,6 @@ class Test(unittest.TestCase):
         robot = SortingRobot(self.random_list)
         robot.sort()
         self.assertEqual(robot._list, sorted(self.random_list))
-
-    # def test_stretch_times(self):
-    #     robot = SortingRobot(self.small_list)
-    #     robot.sort()
-    #     self.assertLess(robot._time, 110)
-
-    #     robot = SortingRobot(self.medium_list)
-    #     robot.sort()
-    #     print(robot._time)
-    #     self.assertLess(robot._time, 1948)
-
-    #     robot = SortingRobot(self.large_list)
-    #     robot.sort()
-    #     print(robot._time)
-    #     self.assertLess(robot._time, 27513)
-
-    #     robot = SortingRobot(self.large_varied_list)
-    #     robot.sort()
-    #     print(robot._time)
-    #     self.assertLess(robot._time, 28308)
 
 
 if __name__ == '__main__':
