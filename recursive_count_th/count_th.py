6caa5cfb5f12ef4bc9af6e64783e24c2ab205212
Greg <gsc229@gmail.com>
2020-02-21 13:09:20 -0500

    robot sorted with count sort

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,19 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
     # TBC
-    
-    pass
+    if word == "th":
+        return word
+
+    if word[0].lower() != "t" and word[1].lower() != "h":
+        print(word)
+        count_th(word[2] + word[2:])
+    elif word[len(word)-2].lower() != "t" or word[len(word)-1].lower() != "h":
+        print(f"elif: {word[:len(word)-1]}")
+        count_th(word[:len(word)-1])
+
+
+print(count_th("throw"))
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -81,11 +81,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -96,15 +98,41 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        arr = self._list
+        num_range = {}
+        new_arr = []
+
+        for i in range(0, len(arr) + 1):
+            num_range[i] = 0
+
+        for i in range(0, len(arr)):
+            if arr[i] < 0:
+                print("Error, negative numbers not allowed in Count Sort")
+                return "Error, negative numbers not allowed in Count Sort"
+            num_range[arr[i]] += 1
+            new_arr.append(0)
+        print(num_range)
+
+        for i in range(1, len(num_range)):
+            num_range[i] = num_range[i-1] + num_range[i]
+        print(num_range)
+
+        for i in range(0, len(arr)):
+            new_arr[num_range[arr[i]]-1] = arr[i]
+            num_range[arr[i]] -= 1
+
+        print(new_arr)
+        self._list = new_arr
+
+    # Fill this out
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
