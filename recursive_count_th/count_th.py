8a8f5971da04b607be7db9987109b2a22d72a7db
Gatrrr <davidgatrmacaulay@gmail.com>
2020-02-21 08:42:21 -0800

    finished count_th.py, tests work

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,9 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
-    # TBC
-    
-    pass
+    n = word.find("th")
+    if n == -1:
+        return 0
+    return 1 + count_th(word[n + 2:])
+
+print(count_th("thetheisthebestthingthatthhasthinitithink")) #test
