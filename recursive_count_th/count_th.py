4c76f872300c63143c6c7c28c54b9bac87dc3899
Emily_Richard <richard.emily.e@gmail.com>
2020-02-21 11:52:34 -0500

    count_th.py complete

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,6 +5,18 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     
+    # game plan:
+    # if find occurrence of 'th' in word, slice after occurrence, recurse with remainder of string
+    # returns 1 + recursive function w/ sliced string
+    # base case 'th' not found, return 0
+
     # TBC
-    
-    pass
+
+    if 'th' in word:
+        i = word.find('th')
+        return 1 + count_th(word[i+2:])
+
+    else:
+        return 0
+
+# print(count_th("the cat that sits on the thatch basket"))
