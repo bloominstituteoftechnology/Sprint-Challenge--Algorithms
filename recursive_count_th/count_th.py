00200eaec46e7ff01dea0ec3c2eb9c43b9ef575e
thejohnwatt <j.watt10@gmail.com>
2020-02-21 10:50:25 -0800

    MVP

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,12 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
-def count_th(word):
+def count_th(word, count=0):
+    if len(word) <2:
+        return count
+    if word[0:2] == "th":
+        count += 1
+    return count_th(word[1:], count)
     
-    # TBC
-    
-    pass
+
+print(count_th("thejohnwatt th"))
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,37 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        self.set_light_on()
+
+        
+        while self.light_is_on():
+
+            
+            self.set_light_off()
+
+            
+            while self.can_move_right():
+                self.swap_item()
+                self.move_right()
+
+                
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+
+            self.swap_item()
+
+            
+            while self.can_move_left():
+                self.swap_item()
+                self.move_left()
+
+                
+                if self.compare_item() == -1:
+                    self.swap_item()
+                    self.set_light_on()
+
+            self.swap_item()
 
 
 if __name__ == "__main__":
