558e0c842ad5545cfb8ccd2a2a0618c6c2291fca
Greg <gsc229@gmail.com>
2020-03-03 00:33:21 -0500

    egg_problem

--- Short-Answer/egg_problem.py
+++ Short-Answer/egg_problem.py
@@ -6,8 +6,10 @@ def find_floor(n, bp, floor=0, counter=1):
     print(f"n={n}")
     print(f"floor={floor}")
     print(f"floor-n/(2^counter)= {floor - (n/2**counter)}")
+
     if round(floor, 5) == round(floor - (n/2**counter), 5):
-        print(f"Egg breaks on floor: {floor}")
+        print(
+            f"Egg breaks on floor: {round(floor)} = breakpoint: {bp}")
         return
 
     if floor < bp:
@@ -23,11 +25,11 @@ def find_floor(n, bp, floor=0, counter=1):
         find_floor(n, bp, floor, counter)
 
 
-n = 100000000000000000000000000000
-break_point = random.randrange(1, n)
+n = 10000000000000
+break_point = random.randrange(500, n)
 print(break_point)
-find_floor(n, break_point)
-print(f"=break_point?: {break_point}")
+find_floor(n, 648)
+
 
 # if floor < bp
 # if floor > bp
