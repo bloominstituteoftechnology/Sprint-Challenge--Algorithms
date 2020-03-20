4b6dd07ddf394284a609ba4a7ec87be821c4bb3a
apathyhill <apathyhill@gmail.com>
2020-03-20 14:14:02 -0400

    Rosie LaSota

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,9 +96,29 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
-
+        self.set_light_on()
+        while self.light_is_on():
+            self.set_light_off()
+            self.swap_item()
+            while self.can_move_right():
+                self.move_right()
+                if self.compare_item() == None:
+                    self.swap_item()
+                elif self.compare_item() < 0:
+                    self.swap_item()
+                    self.set_light_on()
+            self.swap_item()
+            print(self._list, self.light_is_on())
+            while self.can_move_left():
+                self.move_left()
+                if self.compare_item() == None:
+                    self.swap_item()
+                elif self.compare_item() > 0:
+                    self.swap_item()
+                    self.set_light_on()
+            if self.compare_item() == None:
+                self.swap_item()
+            
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
