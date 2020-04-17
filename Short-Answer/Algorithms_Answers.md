6964e2f1acabbb31bb9e1c8ed581e100da79f405
Dennis Rudolph <Dennis@Denniss-MacBook-Pro.local>
2020-04-17 13:09:53 -0400

    All tests passing

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,17 @@
 
 ## Exercise I
 
-a)
+a) O(n)
 
 
-b)
+b) O(n log n)
 
 
-c)
+c) O(n)
 
 ## Exercise II
 
+Start at the first floor and drop an egg. If it doesn't break, go up a floor and repeat until the egg breaks.
+When the first egg breaks, that floor number is the f value. 
 
+Time Complexity: 0(n)
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,25 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+word = "thethe"
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+    # Base cases
+    if word == "":
+        return 0
+    if len(word) == 2:
+        if word[0] == "t" and word[1] == "h":
+            return 1
+        else:
+            return 0
+
+    new_word = word[1:]
+
+    if word[0] == "t" and word[1] == "h":
+        return count_th(new_word) + 1
+    else:
+        return count_th(new_word)
+
