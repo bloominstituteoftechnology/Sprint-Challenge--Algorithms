58993b83a4a62bd2176e5b9b3ab7b8ba06ef11e1
Dan Johnson <danimal.johnson@gmail.com>
2020-04-17 09:54:59 -0700

    Short answer qeustions and recursive count

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -1,15 +1,24 @@
-#### Please add your answers to the ***Analysis of  Algorithms*** exercises here.
+#### Please add your answers to the **_Analysis of Algorithms_** exercises here.
 
 ## Exercise I
 
-a)
+a) O(n^3)?
+(A geometric math equation called n^3 times.)
 
+b) O(n^2)
+(A double nested loop)
 
-b)
-
-
-c)
+c) O(n)
+(A recursive function that counts linearly backwards from n to zero.)
 
 ## Exercise II
 
+Let's attack the building like a binary search tree...
 
+1. Go to the middle floor.
+2. Drop an egg.
+   a. If it breaks, go _down_ half the number of floors you did last time.
+   b. If it doesn't, go _up_ half the number of floors you did last time.
+3. Repeat from #2 until you are no longer changing floors.
+
+You will finish your quest in **log(n) time**. (Where n is the height of the building in floors).
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,24 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    search_string = "th"
+    search_length = 2
+    word_length = len(word)
+
+    # TBC (The Base Case)
+    # The string is to short for our substring "th" to fit.
+    if word_length < search_length:
+        return 0
+
+    # Our search string ("th") matches the beginning of
+    # the current sub-string.
+    # Add 1 to the total and scroll forward 2 characters.
+    if (word[0] == 't' and word[1] == 'h'):
+        return 1 + count_th(word[2:])
+
+    # Our search string doesn't match the beginning of the
+    # substring. Move forward a character.
+    return count_th(word[1:])
