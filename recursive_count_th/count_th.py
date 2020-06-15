b8c99437035ef8d1106da826cb9b13d2e95096d6
ElisabethShah <ElisabethShah@users.noreply.github.com>
2020-06-14 18:45:23 -0700

    Recursive count th.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,23 @@
-'''
+"""
 Your function should take in a single parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function should return a count of how many occurences of ***"th"*** occur
+within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
-'''
+"""
+
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    """
+    Counts the number of appearances of the lowercase substring 'th' in a
+    string 'word'.
+    """
+
+    index = word.find('th')
+
+    # Base case - no such substrings found.
+    if index == -1:
+        return 0
+
+    # Recursive case - one substring found; look for addition substrings in
+    # what remains of the input string.
+    return count_th(word[index + 2:]) + 1
