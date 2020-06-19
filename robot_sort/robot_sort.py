e52c6cda98d4d89b38712c43c1c88b8c7a44d970
Wyatt Harrell <wyattaharrell@gmail.com>
2020-06-19 14:20:07 -0400

    Clean up code comments for better readability

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -105,24 +105,16 @@ class SortingRobot:
 
         #     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-        counter = 0
         while self.can_move_right():
 
             if self.compare_item() == None:
                 self.swap_item()
-                # print("1: HOLDING =", self._item)
 
             self.move_right()
-            # print("MOVE RIGHT. COMPARE WITH:", self._list[self._position])
 
             if self.compare_item() == 1:
                 self.set_light_on()
                 self.swap_item()
-                # print("SWAP: HOLDING =", self._item)
-                # self.move_left()
-                # self.swap_item()
-                # self.move_right()
-                # self.move_right()
                 self.move_left()
                 self.swap_item()
                 self.move_right()
@@ -130,9 +122,7 @@ class SortingRobot:
             else:
                 self.move_left()
                 self.swap_item()
-                # print("MOVE LEFT. SWAP. HOLDING =", self._item)
                 self.move_right()
-                # self.move_right()
 
             if self.can_move_right() == False:
                 if self.light_is_on():
