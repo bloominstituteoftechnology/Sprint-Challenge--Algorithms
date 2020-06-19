696d94f073c5ce37c47e88baf6d3c46f1184e612
SilverMaiden <h.specials@gmail.com>
2020-06-19 15:56:08 -0700

    Completed requirements

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,8 +96,30 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        #Start of function
+        self.set_light_on()
+
+        while self.light_is_on():
+            self.set_light_off()
+
+            self.swap_item()
+            while(self.can_move_right()):
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+                    self.set_light_on()
+
+            while(self.can_move_left()):
+                self.move_left()
+                if self.compare_item() == None:
+                    self.swap_item()
+                    break
+
+            self.move_right()
+
+        while(self.can_move_right()):
+            self.move_right()
+        self.swap_item()
 
 
 if __name__ == "__main__":
