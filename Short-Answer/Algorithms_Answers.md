2ac36f2471805aaa48f8eaa0acde4ac6ba70e9ca
Surfsol <surfsol@protonamil.com>
2020-06-20 14:41:14 -0300

    a

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -1,1 +1,16 @@
 Add your answers to the Algorithms exercises here.
+1. Run time is constant. There is no interating over n, so the size of n would not make a difference for the run time.
+
+2. O(n), the length of n will determine the run time.  Will only loop through n one time.  Other operations are just constant
+
+3.Will be O of n squared.  Because it is recursive.
+
+
+4. Egg 
+
+we can determine f by knowing which eggs broke and from which floor they were dropped.
+Assuming an egg was dropped from each floor.
+We would search for an egg that was broken dropped from the lowest floor.
+By putting the floor with if the dropped egg was broken or not broken in a dictionary {7: broken}
+we could iterate over the dictionary and append add keys of broken eggs to a list.
+then search for the lowest value in the list.  
---
+++ recursive_count_th/count_th.py
@@ -0,0 +1,70 @@
+'''
+Your function should take in a single parameter (a string `word`)
+Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+Your function must utilize recursion. It cannot contain any loops.
+'''
+def count_th(word):
+    first = 0
+    second = 1
+    print(word)
+    #base
+    word = word.lower()
+
+    print(len(word))
+    if len(word) < 2:
+        print('stop')
+        return 0
+    together = word[first] + word[second]
+    if together == 'th':
+        return 1
+        # print('th')
+        
+    new_word = word[1:]
+    print('new word',new_word)
+    return count_th(word[1:])
+   
+
+    # print(len(word))
+    # if len(word) >= 2:
+        
+    #     together = word[first] + word[second]
+    #     print('together', together)
+    #     if together == 'th':
+    #         return 1
+    #     else:
+    #         return 0
+        # print('th')
+        
+    # new_word = word[1:]
+    # print('new word',new_word)
+    # return count_th(word[1:])
+
+# print(count_th('hrthethw'))
+
+# print(len(word))
+#     if len(word) < 2:
+#         print('stop')
+#         return 0
+#     together = word[first] + word[second]
+#     if together == 'th':
+#         return 1
+#     else:
+#         return 0
+#         # print('th')
+        
+#     # new_word = word[1:]
+#     # print('new word',new_word)
+#     return count_th(word[1:])
+
+
+
+
+# count = 0
+#     #split in two
+#     for i in range(1, len(word)):
+#         together = word[i - 1] + word[i]
+#         if together == 'th':
+#             count += 1
+#     #base - until check last two letter
+#         if i+1 == len(word):
+#             return count
