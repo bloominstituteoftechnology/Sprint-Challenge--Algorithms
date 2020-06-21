f9900e0bbda2abf9f94394c42019469ac49d8350
DAP <dlrayjr89@gmail.com>
2020-06-21 17:02:58 +0000

    egg solution is good and robort sort calling the array instead ._list

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,18 +96,18 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        for step in range(len(self._list)):
-            key = self._list[step]
+        for step in range(len(l)):
+            key = l[step]
 
             j = step - 1
 
             #comparing the key with each element on the left until a smaller element is found
-            while j >= 0 and key < self._list[j]:
-                self._list[j + 1] = self._list[j]
+            while j >= 0 and key < l[j]:
+                l[j + 1] = l[j]
                 j = j - 1
 
                 #placing the key after the element
-                self._list[j + 1] = key
+                l[j + 1] = key
 
 
 if __name__ == "__main__":
