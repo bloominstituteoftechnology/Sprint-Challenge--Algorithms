8b2cabaf149347c444dd151b95b68879a4f39c6a
DAP <dlrayjr89@gmail.com>
2020-06-21 17:22:48 +0000

    recursive solved

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,11 +7,17 @@ import time
 start_time = time.time()
 
 def count_th(word):
+    # set the counter var at 0
+    letter_count = 0
+    # set the base case up using length
+    if len(word) <= 1:
+        return 0
 
-    if word == word.count('th'):
-        return 1
-    else:
-        return word.count('th')
+    
+    if word[0] == 't' and word[1] == 'h':
+        letter_count += 1
+    
+    return count_th(word[1:]) + letter_count
 
 end_time = time.time()
 word = 'the ant and the'
