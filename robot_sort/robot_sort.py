7743553a91189f44b8f7445397b2619edd7dab39
Stephen Tanksley <stephen.tanksley@gmail.com>
2020-07-18 13:17:11 -0500

    Wrote out pseudocode.
    Came up with a basic solution and also an opportunity to optimize. Hopefully I'll have enough time to implement at least a basic algorithm before going for stretch.

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -1,3 +1,68 @@
+
+"""
+    U - This looks like an opportunity for a bubble sort. We can start iteratively at the beginning of 
+        the list (on the left side) and check each element against the next element in the list.
+
+        We're going to use the bit of memory for the robot's light as a way of indicating whether we've sorted the list at least 1 time or not. If we make it through a full pass of the list and the robot's light doesn't go off, we're good to go. 
+
+        Otherwise, we reset the robot's light to "OFF" at the end of the loop before it starts on another pass.
+
+    P - 
+        1) Determine if we can move to the right. 
+
+            If so, grab item at the beginning of the list.
+
+
+                2) Determine - 
+
+                    if held item > compared item:
+                        grab compared item and drop held item.
+                        turn the robot's light on.
+                        move to the right
+                    else (if they're already in the right order):
+                        move to the right.
+
+            else:
+                2) Move left in the list until we can't move left anymore.
+                   Turn the robot's light off. This indicates we're at the beginning 
+
+
+        3) Repeat this process until we arrive at the end of the list. The larger values in this    
+           implementation will "bubble" toward the end of the list.
+
+        4) If we make it through an iteration of the entire list and the robot's light doesn't turn on,
+           we can assume that the list is sorted.
+
+
+    E - Write everything out.
+
+    R - I had a thought for a faster version of the bubble sort. Since we're under the assumption that 
+        the robot is a physical thing that needs to be moving back and forth, it doesn't make sense for the robot to move all the way back to the beginning of the list with each iteration.
+
+        With the values of this sort method becoming more sorted over time (larger values bubbling to the right, smaller values bubbling to the left), we could cut our execution time by comparing items from left to right AND right to left. Then we could further decrease the size of our list by changing our starting positions since a full pass going from the start to the end and then back to the start will sort our largest item all the way to the right and our smallest item all the way to the left.
+
+        # before a full range sorting pass - largest value and smallest values are not sorted.
+        items = [ 8, 6, 7, 2, 5, 4, 3, 1]
+
+        # after a full range sorting pass - largest and smallest values are sorted.
+        items = [ 1, 6, 7, 2, 5, 4, 3, 8]
+
+        # We can increase the efficiency of our algorithm by incrementing our starting position and 
+        # decrementing our ending position.
+
+        # before another sorting pass 
+        items = [1, (6, 7, 2, 5, 4, 3), 8] # we only sort from inside the parentheses
+
+        # after another sorting pass
+        items = [1, (2, 6, 5, 4, 3, 7), 8] # we only sort from inside the parentheses
+
+        # incremented again - 
+        items = [1, 2, (6, 5, 4, 3,) 7, 8] # we only sort from inside the parentheses
+
+        etc....
+    """
+
+
 class SortingRobot:
     def __init__(self, l):
         """
@@ -81,11 +146,13 @@ class SortingRobot:
         Turn on the robot's light
         """
         self._light = "ON"
+
     def set_light_off(self):
         """
         Turn off the robot's light
         """
         self._light = "OFF"
+
     def light_is_on(self):
         """
         Returns True if the robot's light is on and False otherwise.
@@ -96,7 +163,16 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
+
+        # 1) Determine if we can move to the right and cannot move to the left. This is us at the beginning of the list. If these are true, grab item in front and move to the right.
+        if (self.can_move_right() is True) and self.can_move_left() is False:
+            self.swap_item()
+
+        while self.can_move_right() is True:
+            self.move_right()
+            # compare the values of the items. If the item in front is greater, swap them.
+            if self.compare_item()
+
         pass
 
 
@@ -104,7 +180,8 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
