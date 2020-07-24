65f73b9e75c2b1552ce7f10f196173c2208a1d49
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-07-24 13:38:31 -0400

    Completes robot_sort.py with basic test passing

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -95,9 +95,75 @@ class SortingRobot:
     def sort(self):
         """
         Sort the robot's list.
+
+        You may NOT store any variables. (=)
+        You may NOT access any instance variables directly. (self._anything)
+        You may NOT use any Python libraries or class methods. (sorted(), etc.)
+        You may define robot helper methods, as long as they follow all the rules.
         """
         # Fill this out
-        pass
+
+        # What do we know?
+            # has method `can_move_right()` which returns false if robot is at end of list
+            # has method `can_move_left()` which returns false if robot is at beginning of list
+            # has method `move_right()` which moves to the right by 1 increment
+            # has method `move_left()` which moves to the left by 1 increment
+            # can `swap_item()` which swaps currently held item with list item in front
+            # `compare_item()` will return 1 if item in fron of robot is greater, -1 if less, 0 if equal
+            # has methods for its light as well but the other methods don't seem to depend on this ...
+            # Default position is 0
+
+        # First pass approach:
+        # Start at beginning of list and compare
+        # If return is None (item > None), swap and move right. Else, move right.
+        # Then, move to right and compare
+        # If -1, then swap again and move right
+        # If 1 or 0, move to right and compare
+        # Do this until end of list, and should be holding smallest item
+        # Next, iterate back to beginning of list (should be holding smallest item) and compare
+        # If -1, move right; if 1 swap and move right; if none swap and move right
+
+        
+        # Adding base case for when robot is at end of list
+        if not self.can_move_right():
+            return
+
+        # in beginning, able to move right
+        while self.can_move_right():
+            # Compare
+            if self.compare_item() == None:
+                # swap and move right
+                self.swap_item()
+                self.move_right()
+
+            # if item is greater or equal
+            if self.compare_item() == 1 or self.compare_item() == 0:
+                # skip it, move right
+                self.move_right()
+            # otherwise, item is less than
+            else:
+                # so swap and move right
+                self.swap_item()
+                self.move_right()
+
+        # once at end, should have smallest item, but can have it work
+        # back to beginning and check along the way 
+        while self.can_move_left() and self.compare_item() != None:
+            # Compare
+            if self.compare_item() == 1:
+                self.swap_item()
+                # keep moving left
+                self.move_left()
+
+            else:
+                # skip and move left
+                self.move_left()
+
+        # Now, robot should be back at beginning of list and holding smallest item, so
+        # we can swap and start over again
+        self.swap_item()
+        self.move_right()
+        self.sort()
 
 
 if __name__ == "__main__":
@@ -106,7 +172,9 @@ if __name__ == "__main__":
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
-    robot = SortingRobot(l)
+    small_list = [5, 4, 3, 2, 1]
+
+    robot = SortingRobot(small_list)
 
     robot.sort()
     print(robot._list)
