0629354b9e54bb27e453f17677f1b0d7d6d59e27
shengjiyang <samswank@gmail.com>
2020-07-24 10:27:21 -0600

    compare adjacent method added

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -76,6 +76,19 @@ class SortingRobot:
         else:
             return 0
 
+    def compare_adjacent(self):
+        if self._list[self._position] > self._list[self._position + 1]:
+            return 1
+
+        elif self._list[self._position] < self._list[self._position + 1]:
+            return -1
+
+        elif self._list[self._position] is None:
+            print(f"self._item is {self._list[self._position]}")
+
+        elif self._list[self._position + 1] is None:
+            print(f"The item to the right is {self._list[self._position + 1]}")
+
     def set_light_on(self):
         """
         Turn on the robot's light
@@ -104,9 +117,26 @@ if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
 
+    # SortingRobot(l).sort()
+    # Bubble sort seems like the most reasonable option given
+    # the constraints.
+
+    # Potential hangups:
+        # The None value
+        # Having an odd number of elements in a list.
+
+    # while Robot can move right:
+        # check the two adjacent values to begin with,
+        # if value to right is smaller,
+        # swap with value on left.
+        # else do nothing.
+
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
+    print(robot.compare_adjacent())
+
     robot.sort()
+    print(len(robot._list))
     print(robot._list)
