4c79464803fcfe61b28124c58269d6eaa8f1bf62
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-07-24 09:30:26 -0700

    count th complete

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,5 +6,11 @@ Your function must utilize recursion. It cannot contain any loops.
 def count_th(word):
     
     # TBC
+    if len(word) < 2:
+        return 0
+    elif word[0:2] == 'th':
+        return count_th(word[1:]) + 1
+    else:
+        return count_th(word[1:])
+
     
-    pass
