da6637f0fbc849818cc5ba14fa2a0c26be729ef8
jpeone <jesse.gene.peone@gmail.com>
2020-08-14 17:51:21 -0700

    off by one index error on the front of the word, fixed, passing all tests

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,7 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
+    # print(word)
     if len(word) < 2:
         return 0
     elif len(word) == 2:
@@ -13,7 +13,8 @@ def count_th(word):
         else:
             return 0
     else:
-        front = word[:1] #slice of the first two letters
+        front = word[:2] #slice of the first two letters
+        print(front)
         if front == 'th':
             back = word[2:] #because if front is th, we can skip the second char
             return 1 + count_th(back)
