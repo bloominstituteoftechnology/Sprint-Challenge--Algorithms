901fe5526e0f93904e8164a1ce03322892bf0bf0
Cameron Moore <cmrnmr@gmail.com>
2020-08-18 22:52:40 -0400

    failing tests

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -6,10 +6,18 @@ Your function must utilize recursion. It cannot contain any loops.
 
 
 def count_th(word):
-    # if t and h occur consecutively at the beginning count and return 1 occurence
+    count = 0
+    # if t and h occur consecutively at the beginning count up 1 occurence
     if word[0] == 't' and word[1] == 'h':
         # recursive call will search consecutive occurences beginning at the 2nd index of the last word
-        # word --> ord --> rd
-        return 1 + count_th(word[1:])
+        # each recurse will successively shorten the original input, word --recurse--> ord --recurse--> rd
+        count += 1
+        return count + count_th(word[1:])
 
+    elif len(word) < 2:
+        count += 0
+        return count
+    else:
+        count += 0
+        return count + count_th(word[1:])
     pass
