ea1906ab561323bcfa0feb05f0dd36853b63c084
Ariunbold <ariun1118@gmail.com>
2020-09-13 21:24:52 -0400

    finished robor sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,7 +97,25 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        
+        if not self.can_move_right():
+            return
+        self.set_light_on()
+       
+
+        while self.light_is_on():
+            self.swap_item()
+            while self.can_move_right():
+                self.move_right()
+                if self.compare_item() == 1:
+                    self.swap_item()
+            while self.compare_item() != None:
+                self.move_left()
+            self.swap_item()
+            if self.can_move_right():
+                self.move_right()
+            else:
+                self.set_light_off()
 
 
 if __name__ == "__main__":
