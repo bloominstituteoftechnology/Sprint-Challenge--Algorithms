cf04582fada8066c148dc1579d0fe512c136c225
Matthew Hidalgo <makeshifthidalgo@hotmail.com>
2020-09-21 08:11:38 -0700

    robot sort finished

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,15 +4,10 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 
-
-
-
-
 def count_th(word):
     count = 0
     if word.find("th") == -1:
         return count
     elif len(word) > 0:
         word.find("th")
-        count += 1
         return 1 + count_th(word.replace("th", " ", 1))
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -91,13 +91,54 @@ class SortingRobot:
         Returns True if the robot's light is on and False otherwise.
         """
         return self._light == "ON"
+    
+
+    def end_of_list(self):
+        while self.light_is_on():
+            if self.compare_item() == 1:
+                self.swap_item()
+                self.set_light_off()
+            elif self.can_move_left():
+                self.move_left()
+            else:
+                self.set_light_off()
+                return self._list
+            
+        while self.light_is_on() is False:
+            if self.can_move_left():
+                self.move_left()
+            else:
+               return self.sort()
+            
+
 
     def sort(self):
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        """
+        1) Grab first item in list, set light on, move to right
+        2) If item in list is greater than what the robot is holding, swap items
+        3) Repeat this until you reach end of the list
+        4) If robot reaches end, swap item at the end of the list if robot's item is greater than item in list and move to the start of the list
+        5) Elif the item isn't greater than the robot's item, move left, repeat step 4. 
+        6) If the robot has an item and returns to the start point after reaching the endpoint and compares each number (can_move_left is False), swap the item with position 0 (which should be none)
+        7) Turn light off, return list
+
+        """
+        self.set_light_on()
+        while self.light_is_on():  
+            if self.compare_item() == None:
+                self.swap_item()
+            if self.can_move_right() is False:
+                self.end_of_list()
+            if self.compare_item() == -1:
+                self.swap_item()
+                self.move_right()
+            else:
+                self.move_right()
+        
+        
 
 
 if __name__ == "__main__":
@@ -105,6 +146,8 @@ if __name__ == "__main__":
     # with `python robot_sort.py`
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+
+    # l = [5, 4, 3, 2, 1]
 
     robot = SortingRobot(l)
 
