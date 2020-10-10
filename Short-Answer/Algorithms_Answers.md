2b3ee0e68eb932a9e9a09cffae978aac56744641
unknown <27julioescalera27@gmail.com>
2020-10-09 20:56:50 -0700

    Still working on robot sort

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,14 +2,40 @@
 
 ## Exercise I
 
-a)
+```python
+a)  a = 0 # O(1)
+    while (a < n * n * n): # O(n^3)
+      a = a + n * n # O(n^2)
 
+# This block of code is O(n^3) since the while loop has 3 n's which basically equals to n^3
+```
 
-b)
+```python
+b)  sum = 0 # O(1) 
+    for i in range(n): # O(n)
+      j = 1 # O(1)
+      while j < n: # O(n) -> O(n^2)
+        j *= 2 # O(1)
+        sum += 1 # O(1)
 
+# This block of code is O(n^2) since the while loop is nested inside the for loop. Both are linear independently 
+# which, added together, makes them n^2.
+```
 
-c)
+```python
+c)  def bunnyEars(bunnies): 
+      if bunnies == 0:
+        return 0 # O(1)
+
+      return 2 + bunnyEars(bunnies-1) # O(n)
+# This block of code is O(n) because the bigger the input, the more recursive call stacks are required
+# for this function. Or another of way of speaking it, the function increases linearly with the the size of the input.
+```
 
 ## Exercise II
 
-
+Simple, we use a binary search algorithm. Specially, we need a pivot, which we would make the middle of the building.
+Then we would see whether we break an egg or not. If so, then we would go to a lower pivot of the building.
+If not, then we would go higher to upper half of the building. We would continue doing this until we hit
+a base case where we find the two floors where one breaks our egg and one doesn't. Then we just return the floor above.
+This solution's runtime would be logarithmic time or O(log n).
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,10 +1,24 @@
-'''
-Your function should take in a single parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
-Your function must utilize recursion. It cannot contain any loops.
-'''
+
 def count_th(word):
-    
-    # TBC
-    
-    pass
+    """
+    Your function should take in a single parameter (a string `word`)
+    Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
+    Your function must utilize recursion. It cannot contain any loops.
+    """
+    # hit a base case where there are no th to count
+    x = "th" in word
+    # if "th" is in word, it would be True
+    if x is True:
+        # when you split only one time, it's gets rid of the first th then split into two list
+        word = word.split("th", 1)
+        # this is where then we splice the list to just only the second half of the string
+        word = word[1:]
+        # we would then use recursion to countinue the splits until we hit our base case
+        # which then we would return all the one's that it has added up
+        # Note: "".join(word) is used to convert the list to a string. "th" in word doesn't work on list
+        # so it will always be False once recused
+        return 1 + count_th("".join(word))
+    else:
+        # Base Case
+        # We either hit this if we no th's in our list or we have nothing literally in string
+        return 0
