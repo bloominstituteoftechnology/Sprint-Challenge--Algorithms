50df0f8544da9795562c44d69dd725a051fe0a14
Pravin Patel <patelpra337@gmail.com>
2020-10-10 13:03:53 -0500

    update Algorithms, count and robot

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,6 +97,11 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
+        if self.light_is_on() != "ON":
+            self.set_light_on()
+        
+        while self.light_is_on():
+            
         pass
 
 
