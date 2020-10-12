0f5b8b21861c2fd63618d621baae645bfeb7eb8a
Kelson Hartle <kelson99hartle@icloud.com>
2020-10-11 20:25:30 -0600

    MVP

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -2,13 +2,13 @@
 
 ## Exercise I
 
-a)
+a) linear time O(n)- because the input size will stay at the same rate the whole time. Which indicates linear.
 
 
-b)
+b) Linearithmic time O(n log n) - because the j will continue to grow in the inner loop O(log n) even though the outer loop is linear.
 
 
-c)
+c) linear O(n) - The function is recursive and will call itself 'n' number of times indicating linear. And adding 2 two the recursive call is also linear which is a constant time operation.
 
 ## Exercise II
 
--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,8 +3,18 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+    
 def count_th(word):
     
-    # TBC
+    print(word[:2])
     
-    pass
+    if  len(word) <= 1:
+        return 0
+    
+    elif word[0] == "t":
+        if word[1] == "h":
+            return 1 + count_th(word[1:])
+        else:
+            return count_th(word[1:])
+    else:
+        return count_th(word[1:])
