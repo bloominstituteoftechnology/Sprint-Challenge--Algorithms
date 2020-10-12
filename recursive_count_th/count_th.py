a5d4793312c9686cfe4fcef351378a4ce0706135
Ash827 <ashley.eddings89@gmail.com>
2020-10-11 23:43:32 -0400

    Sprint Complete with stretch!

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,20 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
+    # if the length of the word is less than 2:
+    if len(word) < 2:
+        # return 0
+        return 0
+    # if the word contains t or h
+    if word[0] == 't' and word[1] == 'h':
+        # return count of 1 + count of 'th' sliced
+        return 1 + count_th(word[2:])
+    else:
+        # else return through word
+        return count_th(word[1:])
     pass
+
+print(count_th("health"))
+print(count_th("fairy"))
+print(count_th("thanks"))
+print(count_th("theatherth"))
