50f0ccccefe7eca2dae2ddbbafc5b28c1ec3e3ad
Chris Filkins <filchyboy@gmail.com>
2020-10-11 19:33:46 -0700

    PEP8 against code

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -1,8 +1,11 @@
 '''
 Your function should take in a single parameter (a string `word`)
-Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
-Your function must utilize recursion. It cannot contain any loops.
+Your function should return a count of how many occurences of ***"th"*** occur
+within `word`. Case matters. Your function must utilize recursion.
+It cannot contain any loops.
 '''
+
+
 def count_th(word):
     # UPER
     # establish the base case
@@ -14,6 +17,6 @@ def count_th(word):
     if word[0:2] == "th":
         return count_th(word[2:]) + 1
 
-    return count_th(word[1:]) 
+    return count_th(word[1:])
 
 print(count_th("th"))
