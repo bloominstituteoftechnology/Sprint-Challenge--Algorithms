c8af6eac89452bd3c75e1e16c7e356c809c722ae
AbreuOliver <oliverabreu@icloud.com>
2020-10-12 22:39:23 -0400

    feat(count_th.py): Completes recursive substring count function

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,19 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
-    pass
+
+    # If the overall length of the input word is less than 2, 
+    # the word can contain no occurences of our substring
+    if len(word) < 2:
+        return 0
+
+
+    # If the input word starts with input, add 1 to count,
+    # and continue to count remaining occurences of substring
+    elif word[:2] == "th":
+        return 1 + count_th(word[1:])
+
+    # Count all occurences of the substring
+    # starting from the second index
+    else:
+        return count_th(word[1:])
