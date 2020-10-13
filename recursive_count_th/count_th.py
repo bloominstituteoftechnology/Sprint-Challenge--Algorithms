63c8d779fc082f9f2ac3b967582d81b7792e995c
Veto Ramirez <vetorobert3@gmail.com>
2020-10-12 20:29:16 -0700

    got all tests working.

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -5,29 +5,31 @@ Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
     # see if list has more that 2 elements. If not, return None
-    if len(word) < 2:
-        return 0
+    count = 0
+    #  splitWord = split(word) # spliting the word list
+
+    if len(word) <= 1:
+        return count
 
     # start a count list to hold the occurences of th
 
     # pointer = 0
 
     # if the first letter in "word" is "t", check if next element is "h"
-    else:
-        count = 0
-
-        if len(word) > 2:
-            if word[0] == 't' and word[1] == 'w':
+    elif word[0] == 't' and word[1] == 'h':
         # add to count
-                count += 1
+        count += 1
         # increment the list (or remove 1st item)
-                word.pop(0)
+        # word.pop(0)
 
     # reset count to start at the next element in the list
     # count = count + count_th(word[1:])
 
     # return count
-    return count
+        return count + count_th(word[2:])
+    
+    else:
+        return count_th(word[1:])
 
     # #self.t = None
     # #self.h = None
